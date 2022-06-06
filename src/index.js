a02a7437d153aa5aa953be806706ee542fb26ca2
Pieter Provoost <pieterprovoost@gmail.com>
2022-06-06 21:07:17 +0200

    added geolocation

--- src/index.js
+++ src/index.js
@@ -37,6 +37,8 @@
   }
   L.control.legend({ position: "topright" }).addTo(map);
 
+  L.control.locate().addTo(map);
+  
   async function updateMap() {
     const maxImages = 9;
     const allColors = ["#fff7f3", "#fde0dd", "#fcc5c0", "#fa9fb5", "#f768a1", "#dd3497", "#ae017e", "#7a0177", "#49006a"];
