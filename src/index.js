184253bdeedf86a1cab2ebf8a97b743f59c0cf32
pieterprovoost <pieterprovoost@users.noreply.github.com>
2022-06-06 19:18:21 +0000

    deploy: 999faf26d66bbb574c791f077db5607f75a35f6c

--- src/index.js
+++ src/index.js
@@ -37,8 +37,11 @@
   }
   L.control.legend({ position: "topright" }).addTo(map);
 
-  L.control.locate().addTo(map);
-  
+  L.control.locate({
+    setView: false,
+    keepCurrentZoomLevel: true
+  }).addTo(map);
+
   async function updateMap() {
     const maxImages = 9;
     const allColors = ["#fff7f3", "#fde0dd", "#fcc5c0", "#fa9fb5", "#f768a1", "#dd3497", "#ae017e", "#7a0177", "#49006a"];
