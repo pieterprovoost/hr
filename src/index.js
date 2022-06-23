6a0243a8f7b7b28db3732d7fbe2d2051040627d2
Pieter Provoost <pieterprovoost@gmail.com>
2022-06-23 23:10:32 +0200

    added alerts

--- src/index.js
+++ src/index.js
@@ -50,6 +50,11 @@
     keepCurrentZoomLevel: true
   }).addTo(map);
 
+  L.easyButton('<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 27 20"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></svg>', function(btn, map){
+  console.log($("#alertsModal"))  
+  $("#alertsModal").modal("show");
+  }).addTo(map);
+
   async function updateMap() {
     const maxImages = 9;
     const allColors = ["#fff7f3", "#fde0dd", "#fcc5c0", "#fa9fb5", "#f768a1", "#dd3497", "#ae017e", "#7a0177", "#49006a"];
@@ -99,9 +104,25 @@
     const div = document.getElementsByClassName("legend")[0];
     div.innerHTML = "";
     for (var i = 0; i < usedColors.length; i++) {
-      const label = "-" + i + " min"
+      const label = "-" + i + " min";
       div.innerHTML += '<i style="background-color: ' + usedColors[i] + '"></i>' + labels[i] + '<br/>';
     }
+
+    if (data.alerts && data.alerts.length) {
+      let content = data.alerts.map(alert => {
+        return "<h5>" + alert.headline + "</h5><p>" + alert.description + "</p>";
+      }).join("");
+      document.getElementById("alertsBody").innerHTML = content;
+      document.querySelectorAll(".easy-button-container").forEach(function(el) {
+        el.style.visibility = "visible";
+      });
+    } else {
+      document.querySelectorAll(".easy-button-container").forEach(function(el) {
+        el.style.visibility = "hidden";
+      });
+      document.getElementById("alertsBody").innerHTML = "";
+    }
+
   }
 
   updateMap();
